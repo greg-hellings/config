5fc33e306ed58e8c42cbe30d3cd926afe51756c8
Gregory Hellings <ghelling@redhat.com>
2016-10-26 15:26:18 -0400

    Basic linch-pin functionality

---
+++ vagrant/shared.rb
@@ -0,0 +1,17 @@
+# shared.rb
+# A shared method for allowing me to spin up multiple VMs easily with easily repeated IP addresses
+
+$ip = 2
+
+def vm(config, name, base_box='fedora/24-cloud-base')
+    config.vm.define name do |node|
+        node.vm.box = base_box
+        node.vm.hostname = name + ".box"
+        node.vm.network "private_network", ip: "192.168.8.#{$ip}", netmask: "255.255.255.0"
+        node.vm.synced_folder "../..", "/vagrant", type: "sshfs"
+        node.vm.provision "ansible_local" do |ansible|
+            ansible.playbook = "site.yml"
+        end
+        $ip += 1
+    end
+end
