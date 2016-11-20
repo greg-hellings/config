c2fb06f04c25367b2014f0ae6d41ea4a1799f3db
Gregory Hellings <ghelling@redhat.com>
2016-11-19 19:31:28 -0500

    Improve handling of Ansible

--- vagrant/shared.rb
+++ vagrant/shared.rb
@@ -8,11 +8,17 @@ def vm(config, name, base_box='fedora/24-cloud-base')
         node.vm.box = base_box
         node.vm.hostname = name + ".box"
         node.vm.network "private_network", ip: "192.168.8.#{$ip}", netmask: "255.255.255.0"
-        node.vm.synced_folder "../..", "/vagrant", type: "sshfs"
-        node.vm.provision "ansible" do |ansible|
-            ansible.playbook = "../../site.yml"
-            yield ansible if block_given?
-        end
+        node.vm.synced_folder "../..", "/home/vagrant/dev", type: "sshfs"
         $ip += 1
+        yield node if block_given?
+    end
+end
+
+def ansible(config)
+    config.vm.provision :ansible do |ansible|
+        ansible.limit = "all"
+        ansible.verbose = "-v"
+        ansible.playbook = "../../site.yml"
+        yield ansible if block_given?
     end
 end
