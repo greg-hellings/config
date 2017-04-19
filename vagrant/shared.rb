e6ee131e94bcc33ab6b856839f219fd80281406a
Gregory Hellings <greg.hellings@gmail.com>
2017-04-19 15:43:57 -0500

    Add system-wide Vagrantfile for settings

--- vagrant/shared.rb
+++ vagrant/shared.rb
@@ -3,31 +3,11 @@
 
 $ip = 2
 
-def get_image(base_box)
-	if base_box == 'centos/7'
-		return 'CentOS-7-x86_64-GenericCloud-released-latest'
-	elsif base_box == 'centos/6'
-		return 'CentOS-6-x86_64-GenericCloud-released-latest'
-	elsif base_box == 'fedora/25-cloud-base'
-		return 'Fedora-Cloud-Base-25-compose-latest'
-	elsif base_box == 'fedora/24-cloud-base'
-		return 'FEdora-Cloud-Base-24-compose-latest'
-	end
-end
-
-def get_username(base_box)
-	if base_box == 'centos/7' or base_box == 'centos/6'
-		return 'centos'
-	else
-		return 'fedora'
-	end
-end
-
 def local_setup(node, base_box)
 	node.vm.box = base_box
 	node.vm.network "private_network", ip: "192.168.8.#{$ip}", netmask: "255.255.255.0"
 	$ip += 1
-	node.vm.synced_folder "../..", "/home/vagrant/dev", type: "sshfs"
+	node.vm.synced_folder "../..", "/home/vagrant/dev", type: "sshfs", sshfs_opts_append: "-o idmap=user"
 end
 
 def vm(config, name, base_box='fedora/25-cloud-base')
@@ -41,29 +21,20 @@ def vm(config, name, base_box='fedora/25-cloud-base')
         node.vm.hostname = name + ".box"
 		node.vm.synced_folder ".", "/vagrant", disabled: true
 		node.vm.provider :openstack do |os, override|
-			os.openstack_auth_url = ENV['OS_ENDPOINT']
-			os.username = ENV['OS_USERNAME']
-			os.password = ENV['OS_PASSWORD']
-			os.tenant_name = ENV['OS_TENANT']
-			os.flavor = ENV['OS_NOVA_FLAVOR']
-			os.floating_ip_pool = ENV['OS_FLOATING_NETWORK']
-			os.networks = [{ name: ENV['OS_NETWORK'] }]
-			os.image = get_image(base_box)
-			os.server_create_timeout = 900
-			override.ssh.username = get_username(base_box)
 			override.vm.synced_folder "../..", "/home/" + get_username(base_box) + "/dev", type: "sshfs"
+			os.image = get_image(base_box)
+			override.ssh.username = get_username(base_box)
 			os.server_name = 'pet-' + ENV['USER'] + '-' + name
 		end
-
-        yield node if block_given?
-    end
+		yield node if block_given?
+	end
 end
 
 def ansible(config)
-    config.vm.provision :ansible do |ansible|
-        ansible.limit = "all"
-        ansible.verbose = "-v"
-        ansible.playbook = "../../site.yml"
-        yield ansible if block_given?
-    end
+	config.vm.provision :ansible do |ansible|
+		ansible.limit = "all"
+		ansible.verbose = "-v"
+		ansible.playbook = "../../site.yml"
+		yield ansible if block_given?
+	end
 end
