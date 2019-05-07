bcdd8de47aa76ab21a7bbc8f1b09fb788a4ab597
Gregory Hellings <greg.hellings@gmail.com>
2019-05-07 14:28:31 -0500

    Update java-devel tests
    Should have updated tests before I did this

--- mkgreg/roles/java-devel/molecule/shared/tests/test_package_install.py
+++ mkgreg/roles/java-devel/molecule/shared/tests/test_package_install.py
@@ -2,8 +2,7 @@ import pytest
 
 
 @pytest.mark.parametrize('name', [
-    ('java-1.8.0-openjdk-headless'),
-    ('java-11-openjdk-headless'),
+    ('java-latest-openjdk-headless'),
     ('maven')
 ])
 def test_package_installed(host, name):
